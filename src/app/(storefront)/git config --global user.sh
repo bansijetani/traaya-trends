7bb5d8cc67f593a02d7ec5c689563ea9ac69691b
bansijetani <bansijetani22@gmail.com>
2026-03-18 15:41:18 +0100

    Upload All settings to original domain

---
+++ src/app/(storefront)/git config --global user.sh
@@ -0,0 +1,44 @@
+git config --global user.email "bansijetani22@gmail.com"
+git config --global user.name "bansijetani"
+git commit -m "Initial launch of Traaya Trends homepage"
+
+
+# 1. Initialize Git
+git init
+
+# 2. Add all your new files
+git add .
+
+# 3. Save the snapshot
+git commit -m "Homepage image issue solve"
+
+# 4. Rename branch
+git branch -M main
+
+# 5. Connect to GitHub (REPLACE 'YOUR_USERNAME' with your actual GitHub username)
+git remote add origin https://github.com/bansijetani/traaya-trends.git
+
+# 6. Push to the cloud
+git push -u origin main
+
+
+git add .
+git commit -m "Update homepage images and fix image config"
+git push
+
+
+sanity API Token for admin
+skUC2yZOOtJDejglAvikJGqA6jPh52SNgpa66ldWRZ61He9mjaeJWTI21gZwR4En3VNfjfIZWuJQsYu2HgM0Z5FnEctPYpMkBLtQ965fqPwrNr7u42mwMpQz4wpaHEh23N3wiRk9FUstfr5DXrllUBDUJo53T0Bo8YixmE5GSWNzc9UTTGF1
+
+
+Set up Real Email Notifications (Make the "We sent an email" text true).
+
+
+colors
+#0C123C
+#97AED8
+#EDE8D0
+
+
+
+BmqZ/uQY+S4x8Kfbi7Fro+QzKKFN5Q9Re05+WRXmRRk=
